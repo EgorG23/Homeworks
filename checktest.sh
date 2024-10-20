875ce1323f7cc9aa74fd65917ee8bde26b1ad4c2
EgorG23 <146130090+EgorG23@users.noreply.github.com>
2024-10-20 23:04:53 +0300

    Update checktest.sh
    "slightly" changes

--- checktest.sh
+++ checktest.sh
@@ -1,59 +1,30 @@
-#!/bin/bashc
+#!/bin/bash
 
 name_of_dir=$1
 directory="$(pwd)/$name_of_dir"
 backup="backup"
-sizeForTest_MB=600  # Минимальный размер в MB для тестов
-numFiles=100     # Количество файлов для генерации
-
-if [ ! -d "directory" ]; then
-    mkdir -p "$directory"
-    sudo mount -o loop limited_size.img $name_of_dir
-    df -h $name_of_dir
-fi
-
-if [ ! -d "$backup" ]; then
-    mkdir "$backup"
-fi
-
-create_file() {
-    filename="$1"
-    filesize_mb="$2"
-    sudo dd if=/dev/zero of="$filename" bs=1M count="$filesize_mb" > /dev/null 2>&1
-    sudo mv "$filename" "$directory"
-}
-
-create_testfiles() {
-    echo "Создание $numFiels файлов в папке $directory"
-    for i in $(seq 1 "$numFiles"); do
-        name="testfile_$i.log"
-        size_mb=$((sizeForTest_MB / numFiles))
-        create_file "$name" "$size_mb"
-    done
-}
 
 testcase_1() {
-    create_testfiles
-    echo "Тест 1: Порог 70%, 5 файлов в архив"
-    ./script "$directory" 70 5 backup
+    echo -e "\n------------------------------------------------------\nTest 1: 600MB, 20 files, Limit 70%, 5 files must be archived\n"
+    ./script "$directory" 1000 20 70 5 backup
 }
 
 testcase_2() {
-    create_testfiles
-    echo "Тест 2: Порог 80%, 10 файлов в архив"
-    ./script "$directory" 80 10 backup
+    echo -e "\n------------------------------------------------------\nTest 2: 150MB, 35 files, Limit is 50%, 10 files must be archived\n"
+    ./script "$directory" 150 35 50 45 backup
 }
 
 testcase_3() {
-    create_testfiles
-    echo "Тест 3: Порог 90%, 20 файлов в архив"
-    ./script "$directory" 90 20 backup
+    echo -e "\n------------------------------------------------------\nTest 3: 550MB, 40 files, Limit is 101%, 20 files must be archived\n"
+    ./script "$directory" 550 40 101 20 backup
 }
 
+timename=$(date +%Y%m%d_%H%M%S)
+name_of_backup="$timename_backup"
+
 testcase_4() {
-    create_testfiles
-    echo "Тест 4: Порог 50%, 15 файлов в архив"
-    ./script "$directory" 50 15 backup
+    echo -e "\n------------------------------------------------------\nTest 4: 700MB, 150 files, Limit is 50%, 21 files must be archived\n"
+    ./script "$directory" 700 150 50 145 name_of_backup
 }
 
 testcase_1
@@ -61,4 +32,4 @@ testcase_2
 testcase_3
 testcase_4
 
-echo "The End"
+echo -e "\nThe End"
